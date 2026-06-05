5c07cb3d3b3ed496e240128df3a36f45b49fd377
jen-mckinney <115019474+jen-mckinney@users.noreply.github.com>
2026-06-05 13:32:53 -0700

    fixes for field required text

--- javascripts/discourse/initializers/wizard-required-helper.js
+++ javascripts/discourse/initializers/wizard-required-helper.js
@@ -1,16 +1,31 @@
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("0.8", (api) => {
-  api.modifyClass("component:wizard-field", {
-    pluginId: "discourse-custom-wizard",
-
-    didInsertElement() {
-      this._super(...arguments);
+export default apiInitializer("1.8.0", (api) => {
+  api.addPostRenderCallback((element) => {
+    const observer = new MutationObserver(() => {
+      const fields = element.querySelectorAll(".wizard-field");
       
-      // if dynamic field config object marks this as required, add a standard utility class
-      if (this.field && this.field.required) {
-        this.element.classList.add("is-required-field");
-      }
+      fields.forEach((fieldEl) => {
+        if (fieldEl.classList.contains("is-required-field") || fieldEl.classList.contains("is-optional-field")) {
+          return;
+        }
+
+        const emberKey = Object.keys(fieldEl).find(key => key.startsWith("__ember_") || key.startsWith("__glimmer_"));
+        const componentInstance = emberKey ? fieldEl[emberKey]?.component : null;
+
+        const fieldData = componentInstance?.field || componentInstance?.args?.field;
+
+        if (fieldData?.required) {
+          fieldEl.classList.add("is-required-field");
+        } else {
+          fieldEl.classList.add("is-optional-field");
+        }
+      });
+    });
+
+    const targetNode = document.querySelector(".custom-wizard") || document.body;
+    if (targetNode) {
+      observer.observe(targetNode, { childList: true, subtree: true });
     }
   });
 });
